11ba44271f2acafadfe8e2e9f00365c8a73bec12
yoyurec <yoyurec@gmail.com>
2022-05-13 04:34:21 +0300

    Observer update, bugfixing

--- custom.js
+++ custom.js
@@ -1,13 +1,24 @@
-// Detect search popup opened/closed and toggle CSS flag `is-search-opened`
-const isSearchOpenedClass = 'is-search-opened';
+// Theme init
 const body = document.body;
-const popupContainer = document.querySelector('.ui__modal');
-const observer = new MutationObserver(function () {
-    const searchPopup = document.querySelector('.search-results-wrap');
-    if(searchPopup){
+body.classList.add('solarized-extended-theme');
+
+// Detect search popup opened/closed and toggle CSS flag `is-search-opened`
+
+const isSearchOpenedClass = 'is-search-opened';
+// Catch Search opened
+const popupContainer = document.querySelector('.ui__modal-panel');
+const observerOpen = new MutationObserver(function () {
+    const searchPopup = popupContainer.querySelector('.search-results-wrap');
+    if (searchPopup) {
         body.classList.add(isSearchOpenedClass);
-    } else {
-        body.classList.remove(isSearchOpenedClass);
+        this.disconnect();
+        observerClose.observe(popupContainer, { childList: true });
     }
 });
-observer.observe(popupContainer, { subtree: true, childList: true });
+observerOpen.observe(popupContainer, { attributes: true });
+// Catch Search closed
+const observerClose = new MutationObserver(function () {
+    body.classList.remove(isSearchOpenedClass);
+    this.disconnect();
+    observerOpen.observe(popupContainer, { attributes: true });
+});
